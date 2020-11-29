936ff293273b6e3a96b3e08ac33db0dbf9d1929a
Jisu Shin <sjszzang3053@gmail.com>
2020-11-30 00:34:13 +0900

    Update settings.py
    security_key 삭제

--- websocket/settings.py
+++ websocket/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$eyn23%6fby7c9vtazf5ll5!f#avsb4c3t*e-@02=n)y4e@vhe'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
